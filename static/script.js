2fd9df8cacb8d31d79b6dea729a2b5f19f3e9f5e
Daniel Pelaez <daniel.pelaezd@gmail.com>
2020-04-01 19:18:39 -0300

    primera version de todos los archivos

---
+++ static/script.js
@@ -0,0 +1,66 @@
+
+//asigno el evento onclick a cada funcion, es lo mismo que hacer onclick en cada button en el html y 
+//getelementbyID aca y correr la funcion..es mejor asi porque queda toda la lògica acà.
+document.querySelector("#hit-button").addEventListener('click',bjHit);
+document.querySelector("#stand-button").addEventListener('click',bjStand);
+document.querySelector("#deal-button").addEventListener('click',bjDeal);
+
+//creo un objeto que tiene los ID de los divs y de los span de cada box
+let blackJackGame ={
+    'you' : {'scoreSpan' : '#your-result','div' : '#your-box','score':0},
+    'dealer' : {'scoreSpan' : '#dealer-result','div' : '#dealer-box','score':0},
+    'cards' : ['2','3','4','5','6','7','8','9','10','J','Q','K','A'],
+    'cardsMap' : {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10,'A':[1,11]},
+};
+//creo dos objetos para acceder rapidamente al jugador o al dealer
+const YOU = blackJackGame['you'];
+const DEALER = blackJackGame ['dealer'];
+const cards = blackJackGame ['cards'];
+const hitSound = new Audio('assets/sounds/swish.m4a');
+const loseSound = new Audio('assets/sounds/aww.mp3');
+const winSound = new Audio('assets/sounds/cash.mp3');
+
+//classes in Javascript (we need the transpiler for old browsers)
+/*
+class User {
+   constructor (){
+       score
+   }
+
+}  */
+
+function bjHit(){
+   const yourCard= randomcard(); 
+   showCard(yourCard,YOU);
+   showScore(yourCard,YOU);
+}
+function bjStand(){
+    alert('STAND');
+}
+function bjDeal(){
+    let dealerCard = randomcard();
+    showCard(dealerCard,DEALER);
+    showScore(dealerCard,DEALER);
+
+}
+
+function randomcard(){
+    return cards[Math.floor(Math.random()*13)];
+}
+
+function showCard(card,player){    
+let cardImage = document.createElement('img');
+cardImage.src ='assets/images/'+card+'.png';
+cardImage.height = 140;
+document.querySelector(player['div']).appendChild(cardImage);
+hitSound.play();
+}
+
+function showScore(card,player){
+player['score']+=blackJackGame['cardsMap'][card];
+document.querySelector(player['scoreSpan']).textContent = player['score'];
+}
+
+
+
+
